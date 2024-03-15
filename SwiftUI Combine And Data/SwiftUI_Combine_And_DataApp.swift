17dd5bd5605e25c312d58bc0104f667fcc3bba63
avalevich <alyaxey.valevich@allegro.com>
2024-03-15 15:08:10 +0100

    Initial Commit

---
+++ SwiftUI Combine And Data/SwiftUI_Combine_And_DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Combine_And_DataApp.swift
+//  SwiftUI Combine And Data
+//
+//  Created by Alyaxey Valevich on 15/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Combine_And_DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
